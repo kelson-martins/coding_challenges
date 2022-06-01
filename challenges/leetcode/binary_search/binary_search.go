cb38e7507c44aae7228da6c8bff116ab376b95e7
Kelson Martins <kelson.martins@outlook.com>
2022-06-01 05:53:27 +0100

    adding recursive approach and readme for binary search problem

--- challenges/leetcode/binary_search/binary_search.go
+++ challenges/leetcode/binary_search/binary_search.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(search([]int{-1, 0, 3, 5, 9, 12}, 9))
+	fmt.Println(searchRecursive([]int{-1, 0, 3, 5, 9, 12}, 9))
 }
 
 // iterative approach
@@ -28,4 +28,26 @@ func search(nums []int, target int) int {
 	}
 
 	return -1
+}
+
+func searchRecursive(nums []int, target int) int {
+
+	return (recursive(nums, target, 0, len(nums)-1))
+}
+
+func recursive(nums []int, target int, left int, right int) int {
+
+	pivot := left + (right-left)/2
+
+	if left > right {
+		return -1
+	}
+
+	if nums[pivot] == target {
+		return pivot
+	} else if nums[pivot] < target {
+		return recursive(nums, target, pivot+1, right)
+	} else {
+		return recursive(nums, target, left, pivot-1)
+	}
 }
